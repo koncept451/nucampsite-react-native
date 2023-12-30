69330e5c3eaa4aa6f30b5900d3bd8c4c3773dc15
Julian Almeida <jalmeida720@gmail.com>
2023-12-30 07:41:23 -0500

    Navigation Part 1

---
+++ screens/MainComponent.js
@@ -0,0 +1,46 @@
+import { View } from "react-native";
+import CampsiteInfoScreen from "./CampsitesInfoScreen";
+import DirectoryScreen from "./DirectoryScreen";
+import { Platform } from "react-native";
+import { Constants } from "expo-constants";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const DirectoryNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator 
+            initialRouteName='Directory' 
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#5637DD'
+                }, 
+                headerTintColor: '#fff'
+            }}
+        >
+            <Stack.Screen
+                name="Directory"
+                component={DirectoryScreen}
+                options={{title: 'Campsite Directory'}}
+            />
+            <Stack.Screen 
+                name="CampsiteInfo" 
+                component={CampsiteInfoScreen} 
+                options={({ route }) => ({
+                    title: route.params.campsite.name
+                })
+                } 
+            />   
+        </Stack.Navigator>
+    )
+}
+
+const Main = () => {
+
+    return (<View style={{ flex: 1, paddingTop:Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+                <DirectoryNavigator />
+            </View>
+    )
+};
+
+export default Main;
+
